9c9157419908c12c932e548a0165465883e9647d
lucasjmdeijkers <lucas.deijkers@proton.me>
2025-12-03 21:32:45 +0100

    started moving logic into generalisable function

--- shamirs.py
+++ shamirs.py
@@ -1,25 +1,49 @@
 import random
 from random import randrange, seed
 
-SECRET = 10
-THRESHOLD = 3
-
-secret_coord = tuple([0,SECRET])
-
-print(secret_coord)
-
 #TODO: Use security safe pseudo-RNG from module 'secrets'
 
 random.seed()
 
-coefficients = []
+def generate_shares(secret, threshold, shares):
+    """
+    This function simple coordinate shares to unlock an integer secret at a threshold number of shares.
 
-for i in range(0,THRESHOLD):
-    coefficients.append(random.randrange(1, 10))
+    :param secret:
+    :param threshold:
+    :param shares:
+    :return:
+    """
 
-print(coefficients)
+    secret_coord = tuple([0,secret])
 
-print(f'{coefficients[0]}x^2 + {coefficients[1]}x + {coefficients[2]}')
+    polynomial_degree = threshold - 1
+
+    coefficients = [secret]
+
+    for i in range(0, polynomial_degree):
+        coefficients.append(random.randrange(1, 10))
+
+    print(coefficients)
+
+    share_values = []
+
+    for i in range(1,shares+1):
+        x_coord = i
+        y_coord = 0
+        for degree in (polynomial_degree, 0, -1):
+
+            y_coord += coefficients[degree]*(x_coord)**degree
+
+        share_value = [x_coord, y_coord]
+
+        share_values.append(tuple(share_value))
+
+    print(share_values)
+
+x = generate_shares(10, 3, 5)
+
+
 
 
 
